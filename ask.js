7afb0180e1f03171ab65a6526eddf06304d4c1e4
Rayen <mhirira@gmail.com>
2026-02-01 21:31:32 +0100

    P.Test

---
+++ ask.js
@@ -0,0 +1,20 @@
+const yesBtn = document.getElementById('yesBtn');
+const noBtn = document.getElementById('noBtn');
+const quizBox = document.getElementById('quiz-box');
+const successMessage = document.getElementById('success-message');
+
+// When she says Yes
+yesBtn.addEventListener('click', () => {
+    quizBox.classList.add('hidden');
+    successMessage.classList.remove('hidden');
+});
+
+// The "Runaway" No Button logic
+noBtn.addEventListener('mouseover', () => {
+    const x = Math.random() * (window.innerWidth - noBtn.offsetWidth);
+    const y = Math.random() * (window.innerHeight - noBtn.offsetHeight);
+    
+    noBtn.style.position = 'absolute';
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+});
